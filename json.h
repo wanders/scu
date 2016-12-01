44724d90dd1e7ef3b0fd2c7ad37871c369762642
Fredrik Appelros <fredrik.appelros@gmail.com>
2016-12-01 15:12:11 +0100

    SCU 0.2
    * Replaced CUnit
    * Started using binary suites instead of shared objects
    * Added JSON based protocol between suites and runner
    * Rewrote test runner in Python

---
+++ json.h
@@ -0,0 +1,95 @@
+#ifndef _JSON_H_
+#define _JSON_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define _SCU_JSON_STRING_LENGTH 1024
+
+#define _SCU_ASCII_BACKSLASH 92
+#define _SCU_ASCII_DOUBLE_QUOTE 34
+
+static inline int __attribute__((used))
+json_true(int fd)
+{
+	return write(fd, "true", 4);
+}
+
+static inline int __attribute__((used))
+json_false(int fd)
+{
+	return write(fd, "false", 5);
+}
+
+#define json_boolean(stream, val) ((val) ? json_true(stream) : json_false(stream))
+
+static inline void __attribute__((used))
+json_escape_string(char *out, const char *in)
+{
+	size_t out_i = 0;
+	for (size_t in_i = 0; (in[in_i] != 0) && (in_i < _SCU_JSON_STRING_LENGTH - 1); in_i++) {
+		if (in[in_i] == _SCU_ASCII_BACKSLASH || in[in_i] == _SCU_ASCII_DOUBLE_QUOTE)
+			out[out_i++] = _SCU_ASCII_BACKSLASH;
+		out[out_i++] = in[in_i];
+	}
+	out[out_i] = 0;
+}
+
+static inline int __attribute__((used))
+json_string(int fd, const char *value)
+{
+	char escaped[_SCU_JSON_STRING_LENGTH];
+	json_escape_string(escaped, value);
+	return dprintf(fd, "\"%s\"", escaped);
+}
+
+static inline int __attribute__((used))
+json_integer(int fd, int value)
+{
+	return dprintf(fd, "%d", value);
+}
+
+static inline int __attribute__((used))
+json_real(int fd, double value)
+{
+	return dprintf(fd, "%f", value);
+}
+
+static inline int __attribute__((used))
+json_object_start(int fd)
+{
+	return write(fd, "{", 1);
+}
+
+static inline int __attribute__((used))
+json_object_key(int fd, const char *key)
+{
+	return dprintf(fd, "\"%s\": ", key);
+}
+
+static inline int __attribute__((used))
+json_object_end(int fd)
+{
+	return write(fd, "}", 1);
+}
+
+static inline int __attribute__((used))
+json_array_start(int fd)
+{
+	return write(fd, "[", 1);
+}
+
+static inline int __attribute__((used))
+json_array_end(int fd)
+{
+	return write(fd, "]", 1);
+}
+
+static inline int __attribute__((used))
+json_separator(int fd)
+{
+	return write(fd, ", ", 2);
+}
+
+#endif
