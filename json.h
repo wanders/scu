e243a05fe11b7e707df1166cb0d96940fdb64eeb
Fredrik Appelros <fredrik.appelros@proceranetworks.com>
2017-03-14 14:04:28 +0100

    Escape JSON strings
    We need to escape any string that we intend to put into the JSON events,
    as unescaped backslash and double quote characters will produce unwanted
    results, and most likely break the parser.

--- json.h
+++ json.h
@@ -5,6 +5,11 @@
 #include <string.h>
 #include <unistd.h>
 
+#define _SCU_JSON_STRING_LENGTH 1024
+
+#define _SCU_ASCII_BACKSLASH 92
+#define _SCU_ASCII_DOUBLE_QUOTE 34
+
 static inline int __attribute__ ((used))
 json_true (int fd)
 {
@@ -19,10 +24,24 @@ json_false (int fd)
 
 #define json_boolean(stream, val) ((val) ? json_true (stream) : json_false(stream))
 
+static inline void __attribute__ ((used))
+json_escape_string (char *out, const char *in)
+{
+  size_t out_i = 0;
+  for (size_t in_i = 0; (in[in_i] != 0) && (in_i < _SCU_JSON_STRING_LENGTH - 1); in_i++) {
+    if (in[in_i] == _SCU_ASCII_BACKSLASH || in[in_i] == _SCU_ASCII_DOUBLE_QUOTE)
+      out[out_i++] = _SCU_ASCII_BACKSLASH;
+    out[out_i++] = in[in_i];
+  }
+  out[out_i] = 0;
+}
+
 static inline int __attribute__ ((used))
 json_string (int fd, const char *value)
 {
-  return dprintf (fd, "\"%s\"", value);
+  char escaped[_SCU_JSON_STRING_LENGTH];
+  json_escape_string (escaped, value);
+  return dprintf (fd, "\"%s\"", escaped);
 }
 
 static inline int __attribute__ ((used))
